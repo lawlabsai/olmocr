d84eb95ba2a2909211041f7ff4a1fad76e3f30bf
Jake Poznanski <jakep@allenai.org>
2025-08-25 20:26:29 +0000

    Saving some extra data mixes

--- scripts/train/grpotrainer-beaker.sh
+++ scripts/train/grpotrainer-beaker.sh
@@ -117,6 +117,7 @@ commands = [
     "echo 'Syncing bench data from S3...'",
     "mkdir -p /data/olmOCR-bench",
     "s5cmd sync 's3://ai2-oe-data/jakep/olmocr/olmOCR-bench-snapshot-082225/*' /data/olmOCR-bench/",
+    "s5cmd sync 's3://ai2-oe-data/jakep/grpo_data_mixes/*' /data/jakep/grpo_data_mixes/",
     
     # Build GRPO training command
     "echo 'Starting GRPO training...'",
