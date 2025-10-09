74eb910b9565a61cbafa0e26e880a2f1916cde72
Jake Poznanski <jakep@allenai.org>
2025-10-09 20:31:28 +0000

    Now you can just run pytest . cleanly

--- scripts/run_integration_test.sh
+++ scripts/run_integration_test.sh
@@ -3,4 +3,4 @@
 set -ex
 
 python -m olmocr.pipeline ./localworkspace --pdfs tests/gnarly_pdfs/ambiguous.pdf tests/gnarly_pdfs/edgar.pdf tests/gnarly_pdfs/dolma-page-1.pdf \
-  && pytest tests/test_integration.py
+  && pytest -m "" tests/test_integration.py
