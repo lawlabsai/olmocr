eaf83026d300eb5e378c66f33ef1652f3ebed647
Jake Poznanski <jakep@allenai.org>
2025-10-20 18:43:13 +0000

    Lints

--- olmocr/bench/runners/run_nanonetsocr_2.py
+++ olmocr/bench/runners/run_nanonetsocr_2.py
@@ -1,21 +1,10 @@
-import json
-from typing import Literal
-
-import httpx
 import base64
 import subprocess
-from PIL import Image
-from olmocr.bench.prompts import (
-    build_basic_prompt,
-    build_openai_silver_data_prompt_no_document_anchoring,
-)
+
+import httpx
+
 from olmocr.data.renderpdf import get_pdf_media_box_width_height
-from olmocr.prompts.anchor import get_anchor_text
-from olmocr.prompts.prompts import (
-    PageResponse,
-    build_finetuning_prompt,
-    build_openai_silver_data_prompt,
-)
+
 
 # Logic to set min size from here: https://github.com/NanoNets/Nanonets-OCR2/blob/main/Nanonets-OCR2-Cookbook/image2md.ipynb
 def render_pdf_to_base64png_min_short_size(local_pdf_path: str, page_num: int, target_shortest_dim: int = 2048) -> str:
@@ -64,19 +53,21 @@ async def run_server(
     """
     # Convert the first page of the PDF to a base64-encoded PNG image.
     image_base64 = render_pdf_to_base64png_min_short_size(pdf_path, page_num=page_num, target_shortest_dim=page_dimensions)
-   
+
     # Now use th
     prompt = """Extract the text from the above document as if you were reading it naturally. Return the tables in html format. Return the equations in LaTeX representation. If there is an image in the document and image caption is not present, add a small description of the image inside the <img></img> tag; otherwise, add the image caption inside <img></img>. Watermarks should be wrapped in brackets. Ex: <watermark>OFFICIAL COPY</watermark>. Page numbers should be wrapped in brackets. Ex: <page_number>14</page_number> or <page_number>9/22</page_number>. Prefer using ☐ and ☑ for check boxes."""
-
 
     request = {
         "model": model,
         "messages": [
             {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": [
-                {"type": "image_url", "image_url": {"url": f"data:image/png;base64,{image_base64}"}},
-                {"type": "text", "text": prompt},
-            ]},
+            {
+                "role": "user",
+                "content": [
+                    {"type": "image_url", "image_url": {"url": f"data:image/png;base64,{image_base64}"}},
+                    {"type": "text", "text": prompt},
+                ],
+            },
         ],
         "temperature": temperature,
         "max_tokens": 4096,
